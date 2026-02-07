4accba58414b262bcf039fb29d0c33fd86197772
Saurav Singh <singh.saurav@icloud.com>
2026-02-07 09:23:28 +0530

    added data from json

--- cypress/e2e/framework.cy.js
+++ cypress/e2e/framework.cy.js
@@ -1,32 +1,46 @@
 describe("E2E Test", () =>
 {
+    let testData;
+    beforeEach(() =>
+    {
+        cy.fixture("example").then((data) =>
+        {
+            testData = data;
+        });
+    });
+
     it("First Test", () =>
     {
         cy.visit("https://rahulshettyacademy.com/loginpagePractise/");
-        cy.get("#username").type("rahulshettyacademy");
-        cy.get("#password").type("Learning@830$3mK2");
+        // Accessing data from fixture
+        cy.get("#username").type(testData.username);
+        cy.get("#password").type(testData.passsword); // Note: matches your JSON spelling
         cy.contains("Sign In").click();
         cy.contains("Shop Name").should("be.visible");
     });
 
-    it.only("Second Test", () =>
+    it("Second Test", () =>
     {
-        const productName = "Nokia Edge";
+        // Using productName from fixture
+        const productName = testData.productName;
+
         cy.visit("https://rahulshettyacademy.com/angularpractice/shop");
         cy.contains("Shop Name").should("be.visible");
         cy.get("div.card-body").should("have.length", 4);
+
         cy.get("app-card")
             .filter(`:contains("${productName}")`)
             .then(($el) =>
             {
                 cy.wrap($el).contains("button", "Add").click();
             });
+
         cy.get(".nav-item.active a").should("contain.text", "Checkout ( 1 )");
         cy.get("app-card").eq(0).contains("button", "Add").click();
         cy.get(".nav-item.active a").should("contain.text", "Checkout ( 2 )");
         cy.contains("a", "Checkout").click();
-        let sum = 0; // Ensure sum is initialized outside
 
+        let sum = 0;
         cy.get("tr td:nth-child(4) strong")
             .each(($el) =>
             {
@@ -38,10 +52,11 @@ describe("E2E Test", () =>
                 cy.log("The total sum is: " + sum);
                 expect(sum).to.be.lessThan(200000);
             });
-        cy.contains('button', 'Checkout').click()
-        cy.get("#country").type("India")
-        cy.get(".suggestions ul li a").click()
-        cy.get(".btn-success").click()
-        cy.get(".alert-success").should('contain', 'Success')
+
+        cy.contains("button", "Checkout").click();
+        cy.get("#country").type("India");
+        cy.get(".suggestions ul li a").click();
+        cy.get(".btn-success").click();
+        cy.get(".alert-success").should("contain", "Success");
     });
 });
