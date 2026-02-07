e9c88f9088d6777a1732dbde4cc57ce27d4b8d5b
Saurav Singh <singh.saurav@icloud.com>
2026-02-07 17:08:09 +0530

    added log hider in e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,3 +16,13 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-mochawesome-reporter/register';
+
+// Hide fetch/XHR requests from the command log
+const app = window.top;
+
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+  const style = app.document.createElement('style');
+  style.innerHTML = '.command-name-request, .command-name-xhr { display: none }';
+  style.setAttribute('data-hide-command-log-request', '');
+  app.document.head.appendChild(style);
+}
