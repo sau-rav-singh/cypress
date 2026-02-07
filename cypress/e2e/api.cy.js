463c624206f567f40ac69696cf911df4637278a6
Saurav Singh <singh.saurav@icloud.com>
2026-02-08 05:15:34 +0530

    added api

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -37,7 +37,28 @@ describe('API Mocking Test Suite', () =>
         cy.get('p').should('have.text', 'Oops only 1 Book available');
     });
 
-    it("Get Book Details", () =>
+    it('Mock HTTP Request', function()
+    {
+        cy.visit("https://rahulshettyacademy.com/angularAppdemo/");
+
+        cy.intercept('GET', 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty',
+            (req) =>
+            {
+                req.url = "https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=malhotra"
+
+                req.continue((res) =>
+                {
+                    //expect(res.statusCode).to.equal(403)
+                })
+            }
+        ).as("dummyUrl")
+
+        cy.get("button[class='btn btn-primary']").click()
+        cy.wait('@dummyUrl')
+
+    })
+
+    it("Get Request Test", () =>
     {
         cy.request({
             method: 'GET',
@@ -61,6 +82,36 @@ describe('API Mocking Test Suite', () =>
 
             // 4. Validate headers (optional but recommended)
             expect(response.headers).to.have.property('content-type', 'application/json;charset=UTF-8');
+        });
+    });
+
+   it('Should successfully add a new book with randomized ISBN and Aisle', () => {
+        // Generate random 3 alphabets for ISBN
+        const randomIsbn = Math.random().toString(36).substring(2, 5); 
+        // Generate random 4 digits for Aisle
+        const randomAisle = Math.floor(1000 + Math.random() * 9000).toString();
+        
+        const expectedID = randomIsbn + randomAisle;
+
+        cy.request({
+            method: 'POST',
+            url: 'https://rahulshettyacademy.com/Library/Addbook.php',
+            body: {
+                "name": "Learn Appium Automation with Java",
+                "isbn": randomIsbn,
+                "aisle": randomAisle,
+                "author": "John foer"
+            }
+        }).then((response) => {
+            // Assertions
+            expect(response.status).to.eq(200);
+            expect(response.body).to.have.property('Msg', 'successfully added');
+            
+            // Asserting that the ID returned matches our randomized input
+            expect(response.body.ID).to.eq(expectedID);
+            
+            // Log it to the Cypress runner for visibility
+            cy.log(`Created Book with ID: ${response.body.ID}`);
         });
     });
 });
